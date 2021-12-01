569e62c95246d5a98296f45b2297faf2719bd1b4
vipenl26 <vipenl26@gmail.com>
2021-12-01 18:09:09 +0530

    added tailwind css, navbar and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
+import Footer from './Footer/Footer';
 import Intro from './Intro/Intro'
+import Navbar from './Navbar/Navbar';
 const MainComponent = () => {
+    const [loadingScreen,setLoadingScreen] = useState(true);
+    useEffect(() => {
+        setTimeout(()=>{
+            setLoadingScreen(false);
+        },2800)
+    }, )
     return (
         <React.Fragment>
-            <Intro/>
+            {loadingScreen && <Intro/>}
+            <Navbar/>
+
+            <Footer/>
         </React.Fragment>
     )
 }
