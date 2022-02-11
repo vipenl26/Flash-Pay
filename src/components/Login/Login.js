380ba774d1cb07e9742d2892d465e690961b0a9b
vipenl26 <vipenl26@gmail.com>
2022-02-11 18:44:43 +0530

    added login and sign with backend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,6 +20,10 @@ const Login = (props) => {
   })
   .then((res)=>{
     if(res.status==200)props.setIsLogin(true)
+    return res.json();
+  })
+  .then(data => {
+    alert(data.message)
   })
   .catch((err)=>{
     
@@ -39,48 +43,48 @@ const Login = (props) => {
 
   return (
     <div>
-      <section class="flex justify-center items-center h-screen bg-gray-800">
-        <div class="max-w-md w-full bg-gray-900 rounded p-6 space-y-4">
-          <div class="mb-4">
-            <p class="text-gray-400">Sign In</p>
-            <h2 class="text-xl font-bold text-white">Flash Pay</h2>
+      <section className="flex justify-center items-center h-screen bg-gray-800">
+        <div className="max-w-md w-full bg-gray-900 rounded p-6 space-y-4">
+          <div className="mb-4">
+            <p className="text-gray-400">Sign In</p>
+            <h2 className="text-xl font-bold text-white">Flash Pay</h2>
           </div>
           <div>
             <input
-              class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
               type="text"
-              placeholder="Email"
+              placeholder="User Name"
               id="username"
             />
           </div>
           <div>
             <input
-              class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
               type="text"
               placeholder="Password"
               id="password"
             />
           </div>
           <div>
-            <button onClick={() => {Authenticate(document.getElementById("username").value,document.getElementById("password").value)}} class="w-full py-4 bg-yellow-500 hover:bg-yellow-400 rounded text-sm font-bold text-gray-50 transition duration-200">
+            <button onClick={() => {Authenticate(document.getElementById("username").value,document.getElementById("password").value)}} className="w-full py-4 bg-yellow-500 hover:bg-yellow-400 rounded text-sm font-bold text-gray-50 transition duration-200">
               Sign In
             </button>
           </div>
-          <div class="flex items-center justify-between">
-            <div class="flex flex-row items-center">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-row items-center">
               <input
                 type="checkbox"
-                class="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+                className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
               />
               <label
-                for="comments"
-                class="ml-2 text-sm font-normal text-gray-400"
+              htmlFor="comments"
+                className="ml-2 text-sm font-normal text-gray-400"
               >
                 Remember me
               </label>
             </div>
             <div>
-              <a class="text-sm text-blue-600 hover:underline" href="#">
+              <a className="text-sm text-blue-600 hover:underline" href="#">
                 Forgot password?
               </a>
             </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,53 +1,99 @@
 import React from 'react'
 
 const Signup = () => {
+    const createUser = async(username, password, password1, fullName, email) => {
+        if(password != password1){
+            alert("passwords don't match. Try again")
+            return
+        }
+
+        const data = {username: username.toString(), password: password.toString(), fullName: fullName.toString(), email: email.toString()}
+        const url = 'http://localhost:3001/login'
+        fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'application/json'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+          })
+          .then((res)=>{   
+            return res.json()
+          })
+          .then(data => {
+              alert(data.message)
+          })
+          .catch((err)=>{
+            console.log(err)
+          })
+    }
     return (
-        <div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+        <div className="bg-grey-lighter min-h-screen flex flex-col">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
                     <input 
                         type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="fullname"
+                        id="fullName"
                         placeholder="Full Name" />
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="username"
+                        id="username"
+                        placeholder="User Name" />
 
                     <input 
                         type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="email"
+                        id="email"
                         placeholder="Email" />
 
                     <input 
                         type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="password"
+                        id="password"
                         placeholder="Password" />
                     <input 
                         type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="confirm_password"
+                        id="password1"
                         placeholder="Confirm Password" />
 
                     <button
                         type="submit"
-                        class="w-full text-center py-3 rounded bg-yellow-400 text-white hover:bg-green-dark focus:outline-none my-1"
+                        className="w-full text-center py-3 rounded bg-yellow-400 text-white hover:bg-green-dark focus:outline-none my-1"
+                        onClick={()=>{createUser(document.getElementById("username").value.toString(),
+                        document.getElementById("password").value,
+                        document.getElementById("password1").value,
+                        document.getElementById("fullName").value,
+                        document.getElementById("email").value)}}
                     >Create Account</button>
 
-                    <div class="text-center text-sm text-grey-dark mt-4">
+                    <div className="text-center text-sm text-grey-dark mt-4">
                         By signing up, you agree to the 
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Terms of Service
                         </a> and 
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
+                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
                             Privacy Policy
                         </a>
                     </div>
                 </div>
 
-                <div class="text-grey-dark mt-6">
+                <div className="text-grey-dark mt-6">
                     Already have an account? 
-                    <a class="no-underline border-b border-blue text-blue" href="../login/">
+                    <a className="no-underline border-b border-blue text-blue" href="../login/">
                         Log in
                     </a>.
                 </div>
