bcd5c49bd5d371d513c6aef3e05eaf89e6519f3e
vipenl26 <vipenl26@gmail.com>
2022-02-11 17:04:44 +0530

    fixed login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,42 @@
 import React from "react";
 
 const Login = (props) => {
+  async function Authenticate(x, y) {
+  const url = 'http://localhost:3001/login'
+  const data = {username:x.toString(), password:y.toString()}
+  // Default options are marked with *
+  fetch(url, {
+    method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  })
+  .then((res)=>{
+    if(res.status==200)props.setIsLogin(true)
+  })
+  .catch((err)=>{
+    
+  })
+}
+  
+  // const Authenticate = (x,y)=>{
+  //   fetch("http://localhost:3001/login")
+  //   .then((res)=>{
+  //     console.log(res)
+  //   })
+  //   .catch((err)=>{
+  //     console.log("errrr");
+  //   })
+
+
+
   return (
     <div>
       <section class="flex justify-center items-center h-screen bg-gray-800">
@@ -14,6 +50,7 @@ const Login = (props) => {
               class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
               type="text"
               placeholder="Email"
+              id="username"
             />
           </div>
           <div>
@@ -21,10 +58,11 @@ const Login = (props) => {
               class="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
               type="text"
               placeholder="Password"
+              id="password"
             />
           </div>
           <div>
-            <button onClick={() => props.setIsLogin(true)} class="w-full py-4 bg-yellow-500 hover:bg-yellow-400 rounded text-sm font-bold text-gray-50 transition duration-200">
+            <button onClick={() => {Authenticate(document.getElementById("username").value,document.getElementById("password").value)}} class="w-full py-4 bg-yellow-500 hover:bg-yellow-400 rounded text-sm font-bold text-gray-50 transition duration-200">
               Sign In
             </button>
           </div>
