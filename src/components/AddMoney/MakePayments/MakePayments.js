34b62bb863e745966a55753452ea78323fa95c6a
i-m-reddy <iamrishab7701@gmail.com>
2022-02-19 00:18:48 +0530

    confirm modal added

--- src/components/AddMoney/MakePayments/MakePayments.js
+++ src/components/AddMoney/MakePayments/MakePayments.js
@@ -1,42 +1,162 @@
 import React from 'react'
-import { AiFillSecurityScan } from 'react-icons/ai'
 import {MakePayment,Text} from './MakePaymentsStyle'
+
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+
+
 function MakePayments(props) {
-    const Payment=(values)=>{
-        const url = "http://localhost:3001/AddMoney"
-        const data = {
-            userid: localStorage.getItem("userid"),
-            cardNumber:values.CardNumber,
-            cardName: values.FullName,
-            expiryDate:values.ExpiryDate,
-            cvv: values.CVV,
-            amount: values.Amount
-        }
-        fetch(url,{
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-              'Content-Type': 'application/json'
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
-          }) 
-        .then((res)=>{
-          return res.json()
-        })
-        .then((res)=>{
-          alert(res.message)
-        })
-        .catch((err)=>{
-          console.log(err)
-        })
+  const [open, setOpen] = useState(false)
+  console.log(props.values)
+  const[values,setValues]=useState(props.values)
+
+  const Payment=(values)=>{
+    const url = "http://localhost:3001/AddMoney"
+    const data = {
+        userid: localStorage.getItem("userid"),
+        cardNumber:values.CardNumber,
+        cardName: values.FullName,
+        expiryDate:values.ExpiryDate,
+        cvv: values.CVV,
+        amount: values.Amount
     }
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      }) 
+    .then((res)=>{
+      return res.json()
+    })
+    .then((res)=>{
+      alert(res.message)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+}
+
+
+
+
+
+  function Confirm() {
+  
+    const cancelButtonRef = useRef(null)
+    
+    function inWords (num) {
+      var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+        if ((num = num.toString()).length > 9) return 'overflow';
+        var n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+        if (!n) return; var str = '';
+        str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+        str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+        str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+        str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+        str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'rupees ' : '';
+        return str;
+    }
+    
     return (
-        <MakePayment onClick={()=>Payment(props.values)} ><Text>Make Payment</Text></MakePayment>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+  
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                        
+                        Confirm payment
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to add <b className="text-base font-bold ">{inWords(props.values.Amount)} only</b> from you card <b className="text-base font-bold ">{props.values.CardNumber}</b> ?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => {setOpen(false);return Payment(props.values)}}
+                  >
+                    Confirm
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    )
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    return (
+        <React.Fragment>
+          <MakePayment onClick={()=>setOpen(true)} ><Text>Make Payment</Text></MakePayment>
+          <Confirm></Confirm>
+        </React.Fragment>
     )
 }
 
--- src/components/PaymentPage/PaymentPage.js
+++ src/components/PaymentPage/PaymentPage.js
@@ -1,13 +1,23 @@
 import { deepStrictEqual } from "assert";
 import { DESTRUCTION } from "dns";
 import React from "react";
-import { useState ,useEffect} from "react";
+import { useEffect} from "react";
 // Card, Microsoft, renewal, -69, 24.12.2020, 07:16 AM
+import useForm from './useForm'
+
+
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+
+
 
 const transaction = (type, where, description, amount, date, time) => {
   date = date.toString().replace("T", "  ")
   date = date.replace("Z", " ")
   
+
+
   return (
     <tr class=" bg-gray-900">
       <td className=" sm:p-3 py-2 px-1 border-b border-gray-800">
@@ -100,6 +110,117 @@ const transaction = (type, where, description, amount, date, time) => {
 
 const PaymentPage = () => {
   const [limit, setLimit] = useState(0)
+  const [open, setOpen] = useState(false)
+
+
+  const [MoneyDetails,setDetails]=useForm({dest:"",amount:""})
+
+
+
+
+
+  function Confirm(props) {
+  
+    const cancelButtonRef = useRef(null)
+    
+    function inWords (num) {
+      var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+        if ((num = num.toString()).length > 9) return 'overflow';
+        var n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+        if (!n) return; var str = '';
+        str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+        str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+        str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+        str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+        str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'rupees ' : '';
+        return str;
+    }
+    
+    return (
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+  
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                        
+                        Confirm payment
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to pay <b className="text-base font-bold ">{inWords(MoneyDetails.amount)} only</b> to <b className="text-base font-bold ">{MoneyDetails.dest}</b> ?
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => {setOpen(false);return SendMoney(MoneyDetails.dest,MoneyDetails.amount)}}
+                  >
+                    Confirm
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   const [ls, setLs] = useState([])
   useEffect(()=>{
     const payments = JSON.parse(localStorage.getItem("data")).payments;
@@ -120,7 +241,7 @@ const PaymentPage = () => {
           <input
             type="text"
             className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-            placeholder="Enter Receiptant" id="dest"
+            placeholder="Enter Receiptant" id="dest" name="dest" onChange={setDetails}
           />
           <div className="absolute top-2 right-2">
             <button className="h-10 w-20 text-white rounded-lg bg-yellow-400 hover:bg-yellow-500">
@@ -136,13 +257,14 @@ const PaymentPage = () => {
           </div>
           <input
             type="text"
-            className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+            className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"  name="amount" onChange={setDetails}
             placeholder="Enter Amount" id="amount"
           />
           <div className="absolute top-2 right-2">
-            <button  onClick={()=>SendMoney(document.getElementById("dest").value.toString(),document.getElementById("amount").value.toString())} className="h-10 w-40 text-white rounded-lg bg-yellow-400 hover:bg-yellow-500">
+            <button  onClick={()=>setOpen(true)} className="h-10 w-40 text-white rounded-lg bg-yellow-400 hover:bg-yellow-500">
              Make Payment 
             </button>
+            <Confirm></Confirm>
           </div>
         </div>
       </div>
